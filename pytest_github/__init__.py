d4d0749141a327a4566e1bd85a36535dac5e992a
James Laska <jlaska@ansible.com>
2016-04-25 15:28:44 -0400

    Bump development version

--- pytest_github/__init__.py
+++ pytest_github/__init__.py
@@ -1,5 +1,5 @@
 """Plugin for py.test that associates tests with github issues using a marker."""
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = "James Laska"
 __author_email__ = "<jlaska@ansible.com>"
